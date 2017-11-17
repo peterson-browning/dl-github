12dffdfaec9ec436d235dc5f35e05c33fc1be6fa
peterson-browning <33648046+peterson-browning@users.noreply.github.com>
2017-11-17 00:22:44 -0800

    Delete dlunzip.sh
    Contents of this file are essentially captured in dljson.sh

--- dlunzip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-GIT_ZIP_LOC="https://github.com/geekcomputers/Python/archive/master.zip"
-TMPFILE=thisTemp.zip
-
-
-curl --location --insecure $GIT_ZIP_LOC -o $TMPFILE
-unzip -o -d $pwd\requires $TMPFILE
-rm $TMPFILE
